b3b037c36feda195814e56a98c31ae89d209f77e
Nadhirbenothmen <nadhir.benothmen@esprit.tn>
2026-02-02 15:36:46 +0100

    Update Logo 2

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = ({ className = "h-12" }) => {
   return (
     <img 
-      src="/portfolio-app/public/assets/Logoo.png" 
+      src="/assets/Logoo.png" 
       alt="NBO - BI & Data Engineer" 
       className={className}
     />
